aaabd90479a7f36572417993db0b149a206cccd1
robcabobb <robcabobb@gmail.com>
2022-03-02 08:13:06 -0500

    added helper functions and tests

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: process.env.SECRET,
@@ -22,6 +19,11 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -29,8 +31,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(session(sess));
 
 app.use(require('./controllers/'));
 
